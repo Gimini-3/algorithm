327707ba9d3fc91f45341af4d0d0795182fc3ad3
Gimin Kim <138752849+Gimini-3@users.noreply.github.com>
2026-01-08 00:36:36 +0900

    [Silver III] Title: 조합, Time: 108 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/2407. 조합/조합.java
+++ 백준/Silver/2407. 조합/조합.java
@@ -1,34 +1,26 @@
-import java.math.BigInteger;
-import java.util.Scanner;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.*;
 
-public class Main {
-    static BigInteger answer=new BigInteger("0");
-    static BigInteger[][] arr;
-    public static void main(String[] args){
-        Scanner sca=new Scanner(System.in);
-        int n=sca.nextInt();
-        int m=sca.nextInt();
-        arr=new BigInteger[n+1][m+1];
-        System.out.println( complication(n,m));
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws IOException {
+          BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+         StringTokenizer st =new StringTokenizer(br.readLine());
+        int n= Integer.parseInt(st.nextToken());
+        int m= Integer.parseInt(st.nextToken());
+        BigInteger x= nCk(n,m);
+        System.out.println(x);
     }
 
-    //nCr= n-1Cr-1 + n-1Cr
-    static BigInteger complication(int n,int m){
-        if(arr[n][m]!=null){
-            return arr[n][m];
+    public static BigInteger nCk(int n,int k){
+        k = Math.min(k,n-k);
+        BigInteger big = BigInteger.ONE;
+        for(int i=1;i<=k ;i++){
+           big = big.multiply(BigInteger.valueOf(n-k+i))
+                .divide(BigInteger.valueOf(i));
         }
-        else if(n==m){
-            arr[n][m]=new BigInteger(String.valueOf(1));
-            return arr[n][m];
-        }
-        else if(m==1) {
-            arr[n][m]=new BigInteger(String.valueOf(n));
-            return arr[n][m];
-        }else{
-            BigInteger a=complication(n-1,m-1);
-            BigInteger b=complication(n-1,m);
-            arr[n][m]=a.add(b);
-            return arr[n][m];
-        }
+        return big;
     }
 }
