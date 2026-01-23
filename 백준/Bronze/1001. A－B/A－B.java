70940ea7246742ff967a7a35130d336f607f9aed
Gimin Kim <138752849+Gimini-3@users.noreply.github.com>
2026-01-23 13:37:07 +0900

    [Bronze V] Title: A-B, Time: 172 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        Scanner sca= new Scanner(System.in);
+        int x = sca.nextInt();
+        int y = sca.nextInt();
+        System.out.println(x-y);
+    }
+}
