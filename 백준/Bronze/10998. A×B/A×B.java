9426a47a3d938a6cc880e1af0c16459fc245572c
Gimin Kim <138752849+Gimini-3@users.noreply.github.com>
2026-01-31 23:49:23 +0900

    [Bronze V] Title: A×B, Time: 152 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        Scanner sca = new Scanner(System.in);
+        int n= sca.nextInt();
+        int m = sca.nextInt();
+        System.out.println(n*m);
+    }
+}
