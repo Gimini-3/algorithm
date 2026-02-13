0ee4b1f3dc21fa3b3d6ab5c0546a179dd0cb3312
Gimin Kim <138752849+Gimini-3@users.noreply.github.com>
2026-02-13 13:55:13 +0900

    [Bronze V] Title: 사칙연산, Time: 184 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+
+        Scanner sca =new Scanner(System.in);
+        int a=sca.nextInt();
+        int b=sca.nextInt();
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
