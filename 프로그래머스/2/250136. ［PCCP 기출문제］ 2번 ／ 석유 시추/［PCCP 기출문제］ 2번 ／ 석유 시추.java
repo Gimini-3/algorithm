78d5d815f5c3a1ec28bbd5501b84144233ed96d6
Gimin Kim <138752849+Gimini-3@users.noreply.github.com>
2026-09-20 01:36:49 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 36.42 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -1,74 +1,97 @@
 import java.util.*;
+
 class Solution {
-    
-    static int[] dx= {0,1,-1,0};
-    static int[] dy = {1,0,0,-1};  
+
+    static int[] dx = {0, 1, -1, 0};
+    static int[] dy = {1, 0, 0, -1};
+
     public int solution(int[][] land) {
+
         int answer = 0;
+
+        int n = land.length;
+        int m = land[0].length;
+
+        int[][] visited = new int[n][m];
+        int[] groupSize = new int[n * m + 1];
+
         Deque<Node> deque = new ArrayDeque<>();
-        int[] lev = new int[land.length * land[0].length + 1];
-        int[][] visited= new int[land.length][land[0].length];
-        int level=1;
-        for(int i=0;i<land.length;i++){
-            for(int j=0;j<land[i].length;j++){
-                if(land[i][j]==1&&visited[i][j]==0){
-                    deque.addLast(new Node(i,j,level));
-                    visited[i][j]=level;
-                    lev[level]+=1;
-                    while(true){
-                        if(deque.isEmpty()){
-                            level++;      
-                            break;
-                        }
+
+        int level = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                if (land[i][j] == 1 && visited[i][j] == 0) {
+
+                    deque.addLast(new Node(i, j));
+                    visited[i][j] = level;
+                    groupSize[level]++;
+
+                    while (!deque.isEmpty()) {
+
                         Node now = deque.pollFirst();
-                        for(int t=0;t<4;t++){
-                            int nextX= now.x+dx[t];
-                            int nextY= now.y+dy[t];
-                            if(nextX<0||nextX>=land.length||nextY<0||nextY>=land[0].length)continue;
-                            if(land[nextX][nextY]==0)continue;
-                            if(visited[nextX][nextY]!=0)continue;
-                            visited[nextX][nextY]=level;
-                            lev[level]+=1;
-                            deque.addLast(new Node(nextX,nextY,now.lev));
+
+                        for (int t = 0; t < 4; t++) {
+
+                            int nextX = now.x + dx[t];
+                            int nextY = now.y + dy[t];
+
+                            if (nextX < 0 || nextX >= n ||
+                                nextY < 0 || nextY >= m) {
+                                continue;
+                            }
+
+                            if (land[nextX][nextY] == 0) {
+                                continue;
+                            }
+
+                            if (visited[nextX][nextY] != 0) {
+                                continue;
+                            }
+
+                            visited[nextX][nextY] = level;
+                            groupSize[level]++;
+
+                            deque.addLast(new Node(nextX, nextY));
                         }
                     }
-                }      
-                          
 
-            }
-        }  
-       /** for(int i=0;i<land.length;i++){
-            for(int j=0;j<land[i].length;j++){
-                System.out.print(visited[i][j]+" ");
-            }
-            System.out.println();
-        }*/
-        for(int i=0;i<land[0].length;i++){
-            Set<Integer> set = new HashSet<>();
-            int count=0;
-            for(int j=0;j<land.length;j++){
-                if(visited[j][i]!=0){
-                    set.add(visited[j][i]);
+                    level++;
                 }
             }
-            
-            for(int x : set){
-                count+=lev[x];
-            }
-          //  System.out.println(count);
-            answer=Math.max(answer,count);
         }
+
+        for (int col = 0; col < m; col++) {
+
+            Set<Integer> set = new HashSet<>();
+
+            for (int row = 0; row < n; row++) {
+
+                if (visited[row][col] != 0) {
+                    set.add(visited[row][col]);
+                }
+            }
+
+            int count = 0;
+
+            for (int group : set) {
+                count += groupSize[group];
+            }
+
+            answer = Math.max(answer, count);
+        }
+
         return answer;
     }
-    
-    public class Node{
+
+    static class Node {
         int x;
         int y;
-        int lev;
-        public Node(int x,int y,int lev){
-            this.x=x;
-            this.y=y;
-            this.lev = lev;
+
+        Node(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
     }
 }
