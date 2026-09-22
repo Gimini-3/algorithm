ecab92ab80f79ba9275cfdacbbd505c0c7c8d521
Gimin Kim <138752849+Gimini-3@users.noreply.github.com>
2026-09-23 02:14:22 +0900

    Update 호텔 대실.java

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -4,12 +4,10 @@ class Solution {
     public int solution(String[][] book_time) {
         int answer = 0;
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-        Arrays.sort(book_time,  (a,b)->{
+         Arrays.sort(book_time,  (a,b)->{
             return a[0].compareTo(b[0]);
         });
         
-        int[] rooms= new int[1001];
         for(int i=0;i<book_time.length;i++){
             String[] str1 = book_time[i][0].split(":");
             int total1 = Integer.parseInt(str1[0])*60+ Integer.parseInt(str1[1]);
