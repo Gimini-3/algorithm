27eaa45400cdcdb18350760dd6452ac52980486d
Gimin Kim <138752849+Gimini-3@users.noreply.github.com>
2026-09-24 22:27:43 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 11.54 ms, Memory: 157 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -1,43 +1,38 @@
 class Solution {
     public int[] solution(int[] sequence, int k) {
         int[] answer = new int[2];
-        int start_idx= 0;
-        int end_idx=0;
-        int length=Integer.MAX_VALUE;
-        long sum= sequence[0];
-        
-        while(true){
-           // System.out.println(start_idx+" "+end_idx+" "+sum);
-            
-           
-            if(start_idx>end_idx)break;
-            if(sum>k){
-                sum-=sequence[start_idx];
-                start_idx++;
-            }else if(sum<k){
-                if(end_idx<sequence.length-1){
-                    end_idx++;
-                    sum+=sequence[end_idx];
-                }else if(end_idx==sequence.length-1){
+
+        int start = 0;
+        int end = 0;
+        int minLength = Integer.MAX_VALUE;
+
+        long sum = sequence[0];
+
+        while (start <= end) {
+
+            if (sum == k) {
+                int currentLength = end - start + 1;
+
+                if (currentLength < minLength) {
+                    minLength = currentLength;
+                    answer[0] = start;
+                    answer[1] = end;
+                }
+            }
+
+            if (sum >= k) {
+                sum -= sequence[start];
+                start++;
+            } else {
+                if (end == sequence.length - 1) {
                     break;
                 }
-            }else{
-                if(end_idx-start_idx<length){
-                    System.out.println("check"+start_idx+" "+end_idx+" "+sum);
-                    length=end_idx-start_idx;
-                
-                    answer[0]=start_idx;
-                    answer[1]=end_idx;
-                }
-                if(end_idx<sequence.length-1){
-                    end_idx++;
-                    sum+=sequence[end_idx];
-                }else if(end_idx==sequence.length-1){
-                    break;
-                }
-                
+
+                end++;
+                sum += sequence[end];
             }
         }
+
         return answer;
     }
 }
